9808cbdf9e3d1ffc5d8d62971853a5511d87b8c8
yogaibnu <yogaibnudinataa@gmail.com>
2020-05-11 12:41:39 +0700

    add firebase initialize at index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase');
+require('firebase/firestore');
+
+firebase.initializeApp(
+  {
+    apiKey: "AIzaSyAp5kAMnmwY0zFq6GiQ5FfCK6pIKhhq-Vk",
+    authDomain: "pbf-uas.firebaseapp.com",
+    databaseURL: "https://pbf-uas.firebaseio.com",
+    projectId: "pbf-uas",
+    storageBucket: "pbf-uas.appspot.com",
+    messagingSenderId: "978105850074",
+    appId: "1:978105850074:web:2c849def7c0100e38a6de3"
+  }
+);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
