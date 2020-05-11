b19b59d09f268635f0a9787bc8de127b31ae82b9
yogaibnu <yogaibnudinataa@gmail.com>
2020-05-11 13:04:07 +0700

    add constructor at App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    }
+  }
+
   render() {
     return(<div>Hello</div>)
   }
+
+  
 }
 
 export default App;
