7193ec739d73ad5e45291776b70447006062fa4d
yogaibnu <yogaibnudinataa@gmail.com>
2020-05-11 14:42:26 +0700

    fixing some errors

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -17,48 +17,52 @@ class SiderbarComponent extends React.Component {
 
         const { notes, classes, selectedNoteIndex } = this.props;
 
-        return (
-            <div className={classes.sidebarContainer}>
-                <Button
-                    onClick={this.newNoteBtnClick}
-                    className={classes.newNoteBtn}
-                >{this.state.addingNote ? 'Cancel' : 'New Note'}</Button>
-                {
-                    this.state.addingNote ?
-                    <div>
-                        <input
-                        type='text'
-                        className={classes.newNoteInput}
-                        placeholder='Enter note title'
-                        onKeyUp={(e) => this.updateTitle(e.target.value)}>
-                        </input>
-                        <Button
-                            className={classes.newNoteSubmitBtn}
-                            onClick={this.newNote}
-                        >Submit Note</Button>
-                    </div> :
-                    null
-                }
-                <List>
+        if(notes) {
+            return (
+                <div className={classes.sidebarContainer}>
+                    <Button
+                        onClick={this.newNoteBtnClick}
+                        className={classes.newNoteBtn}
+                    >{this.state.addingNote ? 'Cancel' : 'New Note'}</Button>
                     {
-                        notes.map((_note, _index) => {
-                            return(
-                                <div key={_index}>
-                                    <SidebarItemComponent
-                                    _note={_note}
-                                    _index={_index}
-                                    selectedNoteIndex={selectedNoteIndex}
-                                    selectNote={this.selectNote}
-                                    deleteNote={this.deleteNote}
-                                    ></SidebarItemComponent>
-                                    <Divider></Divider>
-                                </div>
-                            )
-                        })
+                        this.state.addingNote ?
+                        <div>
+                            <input
+                            type='text'
+                            className={classes.newNoteInput}
+                            placeholder='Enter note title'
+                            onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                            </input>
+                            <Button
+                                className={classes.newNoteSubmitBtn}
+                                onClick={this.newNote}
+                            >Submit Note</Button>
+                        </div> :
+                        null
                     }
-                </List>
-            </div>
-        );
+                    <List>
+                        {
+                            notes.map((_note, _index) => {
+                                return(
+                                    <div key={_index}>
+                                        <SidebarItemComponent
+                                        _note={_note}
+                                        _index={_index}
+                                        selectedNoteIndex={selectedNoteIndex}
+                                        selectNote={this.selectNote}
+                                        deleteNote={this.deleteNote}
+                                        ></SidebarItemComponent>
+                                        <Divider></Divider>
+                                    </div>
+                                )
+                            })
+                        }
+                    </List>
+                </div>
+            );
+        } else {
+            return(<div></div>);
+        }
     }
 
     newNoteBtnClick = () => {
