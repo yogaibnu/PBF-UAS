3ea9e328ac982447b697c4ccf27e39721bcb72d3
yogaibnu <yogaibnudinataa@gmail.com>
2020-05-11 16:00:26 +0700

    create function noteUpdate in App.js

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends React.Component {
           selectedNote={this.state.selectedNote}
           selectedNoteIndex={this.state.selectedNoteIndex}
           notes={this.note}
+          noteUpdate={this.noteUpdate}
           ></EditorComponent> : null
         }
       </div>
@@ -55,6 +56,18 @@ class App extends React.Component {
   }
 
   selectNote = (note, index) => this.setState({selectedNoteIndex: index, selectedNote: note});
+
+  noteUpdate = (id, noteObj) => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .doc(id)
+      .update({
+        title: noteObj.title,
+        body: noteObj.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      })
+  }
 }
 
 export default App;
